5b6a325f9183e7896880afe59d95d420f07c0418
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2011-05-26 22:35:53 +0100

    Got things working with jersey, jetty and tomcat

--- src/main/java/uk/co/brotherlogic/busittom/GetStops.java
+++ src/main/java/uk/co/brotherlogic/busittom/GetStops.java
@@ -1,9 +1,5 @@
 package uk.co.brotherlogic.busittom;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -12,28 +8,9 @@ import javax.ws.rs.Produces;
 public class GetStops
 {
    @GET
-   @Produces("test/plain")
-   public String sayHello(String name)
+   @Produces("text/plain")
+   public String sayHello()
    {
-      return "Hello, " + name;
-   }
-
-   public static void main(String[] args) throws IOException
-   {
-
-      final String baseUri = "http://localhost:9998/";
-      final Map<String, String> initParams = new HashMap<String, String>();
-
-      initParams.put("com.sun.jersey.config.property.packages",
-            "com.sun.jersey.samples.helloworld.resources");
-
-      System.out.println("Starting grizzly...");
-      SelectorThread threadSelector = GrizzlyWebContainerFactory.create(baseUri, initParams);
-      System.out.println(String.format(
-            "Jersey app started with WADL available at %sapplication.wadl\n"
-                  + "Try out %shelloworld\nHit enter to stop it...", baseUri, baseUri));
-      System.in.read();
-      threadSelector.stopEndpoint();
-      System.exit(0);
+      return "Hello there";
    }
 }
